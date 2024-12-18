0ee3686d4751e98488fbcf783467318885ec1a11
burnthecores <93864637+burnthecores@users.noreply.github.com>
2024-12-18 16:20:51 +0100

    Update apna_typedef.c

---
+++ structures/apna_typedef.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+
+typedef struct Electronic_student{
+    int roll;
+    float cgpa;
+    char name[100];
+
+} EBIT; // typedef is used to create alias to a structure. ex: EBIT is alias to Electronic_student
+
+int main(){
+    EBIT s1;
+
+}
